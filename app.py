d66e67851291f81f7ac51a6442580401d0340a25
relishLeisure <119769824+relishLeisure@users.noreply.github.com>
2024-01-11 00:35:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-
-from langchain.vectorstores import Chroma
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 import os
-import os
+os.system('pip install -r requirements.txt')
 
 # 设置环境变量
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
@@ -10,6 +7,8 @@ os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 # 下载模型
 os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /root/model/sentence-transformer')
 
+from langchain.vectorstores import Chroma
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from LLM import InternLM_LLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
