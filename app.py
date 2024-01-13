dd9b0fa407a0b2701629779c58c5ea9402c9fe98
relishLeisure <119769824+relishLeisure@users.noreply.github.com>
2024-01-14 01:35:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 
-os.system("cd /usr/local")
-os.system("yum -y install gcc gcc-c++ autoconf automake")
-os.system("yum -y install zlib zlib-devel openssl openssl-devel pcre pcre-devel")
-os.system("./configure")
+
+# os.system("yum -y install gcc gcc-c++ autoconf automake")
+# os.system("yum -y install zlib zlib-devel openssl openssl-devel pcre pcre-devel")
+# os.system("./configure")
 
 #下载glibc2.29
 os.system("cd /usr/local")
@@ -11,6 +11,9 @@ os.system("wget http://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz")
 os.system("tar -zxvf glibc-2.29.tar.gz")
 os.system("cd glibc-2.29")
 os.system("mkdir build && cd build")
+
+os.system("gcc -v")
+
 os.system("../configure --prefix=/usr/local/glibc-2.29")
 os.system("make -j 64")
 os.system("make install")
