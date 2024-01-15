fb4746eed9d4ffd116293a1f04f80f1fad1aeebf
relishLeisure <119769824+relishLeisure@users.noreply.github.com>
2024-01-15 23:50:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ import os
 # 设置环境变量
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 os.system("pip install chromadb==0.3.29")
+os.system("strings /lib/x86_64-linux-gnu/libm.so.6 | grep GLIBC_")
+
 # 下载模型
 os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /home/xlab-app-center/model/sentence-transformer')
 
